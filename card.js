d4454f3f12944ff01e8427a18e4359c8d2a71980
Croluy <croluy@fbi.ac>
2022-04-01 17:58:17 +0200

    small change in prompt messages

--- card.js
+++ card.js
@@ -48,11 +48,11 @@ const text = chalk.italic;
 
 const data = {
     question: defaultc("What do you wish to do next?"),
-    email_q: defaultc("Send me an email?"),
+    email_q: defaultc("Send me an email."),
     email_message: defaultc("Awesome, see you soon at inbox.\n-Croluy\n"),
     pgp_q: defaultc("Download my PGP Key."),
     pgp_message: defaultc("Also feel free to sign my key and reupload it somewhere like: ") + chalk.cyan(`${pgp_upload}`) + defaultc(".\n-Croluy\n"),
-    tip_q: defaultc("Send a tip."),
+    tip_q: defaultc("Send me a tip."),
     tip_message: defaultc("I\'d really apprectiate the opportunity to thank you personally. So please get in touch with me whenever you get the chance.\nYou have all my contacts above.\n\nBitcoin Address:\t") + chalk.cyan(`${btc_address}`) + defaultc("\nEthereum Address:\t") + chalk.cyan(`${eth_address}`) + defaultc("\nMonero Address:\t") + chalk.cyan(`${monero_address}`) + defaultc("\n\nThank you very much for your kind support!\n-Croluy"),
     quit_q: defaultc("Quit this menu."),
     quit_message: defaultc("Thanks for dropping by.\n-Croluy\n"),
